c26d6cc83a7fece5fc0d85d4ec72cf8b018f7184
Gabriel Sene <sene300@gmail.com>
2022-11-22 20:21:45 -0300

    Iterator, forma antiga quando não havia o foreach

--- src/br/com/alura/TestaCursoComAluno.java
+++ src/br/com/alura/TestaCursoComAluno.java
@@ -1,5 +1,8 @@
 package br.com.alura;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class TestaCursoComAluno {
 
 	public static void main(String[] args) {
@@ -21,9 +24,19 @@ public class TestaCursoComAluno {
 		javaCurso.matricula(a3);
 		
 		System.out.println("todos os alunos matriculados nesse curso: ");
-		javaCurso.getAlunos().forEach(alunos-> {
-			System.out.println(alunos);
-		});
+		
+		Set<Aluno>alunos = javaCurso.getAlunos();
+		Iterator<Aluno>iterador = alunos.iterator();
+		while(iterador.hasNext()) {
+			Aluno proximo = iterador.next();
+			System.out.println(proximo);
+			
+		}
+		
+		
+//		javaCurso.getAlunos().forEach(alunos-> {
+//			System.out.println(alunos);
+//		});
 		
 		System.out.println("O aluno: "+a1+" está matriculado?");
 		System.out.println(javaCurso.estaMatriculadoAluno(a1));
